b2d66c8e632f922d443578d3caacb80da0134d70
shikhar Gupta <shikhargupta442003@gmail.com>
2024-08-19 22:42:23 +0530

    controller done

--- VeersaGrp/src/main/java/com/example/VeersaGrp/controller/ProgramController.java
+++ VeersaGrp/src/main/java/com/example/VeersaGrp/controller/ProgramController.java
@@ -1,13 +1,15 @@
 package com.example.VeersaGrp.controller;
 
 
+import com.example.VeersaGrp.dto.ProgramDto;
 import com.example.VeersaGrp.model.Program;
 import com.example.VeersaGrp.service.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/program")
@@ -20,6 +22,15 @@ public class ProgramController {
         this.programService=programService;
     }
 
-    @PostMapping()
-    public ResponseEntity<Program>
+    @PostMapping("{clientId}")
+    public ResponseEntity<ProgramDto> registerProgram(@RequestBody ProgramDto programDto, @RequestParam List<Long> serviceIds ){
+        ProgramDto result=programService.registerProgram(programDto,serviceIds);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{programId}")
+    public ResponseEntity<ProgramDto> findProgram(@PathVariable Long programId){
+        ProgramDto result=programService.findProgram(programId);
+        return new ResponseEntity<>(result,HttpStatus.FOUND);
+    }
 }
--- VeersaGrp/src/main/java/com/example/VeersaGrp/service/ProgramService.java
+++ VeersaGrp/src/main/java/com/example/VeersaGrp/service/ProgramService.java
@@ -1,4 +1,11 @@
 package com.example.VeersaGrp.service;
 
+import com.example.VeersaGrp.dto.ProgramDto;
+
+import java.util.List;
+
 public interface ProgramService {
+    ProgramDto registerProgram(ProgramDto programDto, List<Long> serviceIds);
+
+    ProgramDto findProgram(Long programId);
 }
