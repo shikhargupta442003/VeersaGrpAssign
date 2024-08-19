bd6c7fef68def6fc8d3f7915e7cb95c4addb683c
shikhar Gupta <shikhargupta442003@gmail.com>
2024-08-19 21:24:57 +0530

    mapper done

--- VeersaGrp/src/main/java/com/example/VeersaGrp/mapper/ProgramMapper.java
+++ VeersaGrp/src/main/java/com/example/VeersaGrp/mapper/ProgramMapper.java
@@ -1,4 +1,21 @@
 package com.example.VeersaGrp.mapper;
 
-public class ProgramMapper {
+import com.example.VeersaGrp.dto.ProgramDto;
+import com.example.VeersaGrp.model.Program;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = {ClientMapper.class, ServiceMapper.class})
+public interface ProgramMapper {
+
+    ProgramMapper INSTANCE = Mappers.getMapper(ProgramMapper.class);
+
+    @Mapping(target = "client", source = "client")
+    @Mapping(target = "services", source = "services")
+    Program toEntity(ProgramDto programDto);
+
+    @Mapping(target = "client", source = "client")
+    @Mapping(target = "services", source = "services")
+    ProgramDto toDto(Program program);
 }
--- VeersaGrp/src/main/java/com/example/VeersaGrp/mapper/ServiceMapper.java
+++ VeersaGrp/src/main/java/com/example/VeersaGrp/mapper/ServiceMapper.java
@@ -1,4 +1,21 @@
 package com.example.VeersaGrp.mapper;
 
-public class ServiceMapper {
+import com.example.VeersaGrp.dto.ServiceDto;
+import com.example.VeersaGrp.model.Service;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = {ProgramMapper.class, ClientMapper.class})
+public interface ServiceMapper {
+
+    ServiceMapper INSTANCE = Mappers.getMapper(ServiceMapper.class);
+
+    @Mapping(target = "programs", source = "programs")
+    @Mapping(target = "clients", source = "clients")
+    Service toEntity(ServiceDto serviceDto);
+
+    @Mapping(target = "programs", source = "programs")
+    @Mapping(target = "clients", source = "clients")
+    ServiceDto toDto(Service service);
 }
